40b7c2acc6a054514af3cc8c2f01722e84e32e99
John Blackbourn <johnbillion@gmail.com>
2021-02-11 23:03:11 +0100

    Restructuring, tidying.

--- tests/mu-plugins/mu-plugin.php
+++ tests/mu-plugins/mu-plugin.php
@@ -1,6 +1,10 @@
 <?php
 
-add_action( 'init', function() {
+namespace WPJsonSchemas;
+
+use WP_CLI;
+
+add_action( 'init', function() : void {
 	if ( defined( 'WP_INSTALLING' ) ) {
 		return;
 	}
@@ -16,22 +20,27 @@ if ( ! defined( 'WP_CLI' ) || ! WP_CLI ) {
 	return;
 }
 
-WP_CLI::add_command( 'json-dump', function() {
+function save( array $data, string $dir ) : void {
+	$dir = dirname( ABSPATH ) . '/data/' . $dir;
+
+	if ( ! file_exists( $dir ) ) {
+		mkdir( $dir, 0777, true );
+	}
+
+	foreach ( $data as $i => $item ) {
+		$json = json_encode( $item, JSON_PRETTY_PRINT ^ JSON_UNESCAPED_SLASHES );
+
+		file_put_contents( $dir . '/' . $i . '.json', $json );
+	}
+}
+
+WP_CLI::add_command( 'json-dump post', function() : void {
 	$posts = get_posts( [
 		'posts_per_page' => -1,
 		'orderby'        => 'ID',
 		'order'          => 'ASC',
 	] );
 
-	$dir = dirname( ABSPATH ) . '/data/post/';
-
-	if ( ! file_exists( $dir ) ) {
-		mkdir( $dir, 0777, true );
-	}
-
-	foreach ( $posts as $i => $post ) {
-		$json = json_encode( $post, JSON_PRETTY_PRINT ^ JSON_UNESCAPED_SLASHES );
-
-		file_put_contents( $dir . '/' . $i . '.json', $json );
-	}
+	save( $posts, 'post' );
+} );
 } );
