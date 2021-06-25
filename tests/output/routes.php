462c2c13b4db87dbbaa57aef13633c90a0610fcb
John Blackbourn <johnbillion@gmail.com>
2021-06-25 21:40:48 +0100

    Better naming for the route test data files.

--- tests/output/routes.php
+++ tests/output/routes.php
@@ -6,10 +6,10 @@ $data = get_rest_response( 'GET', '/wp/v2', [] );
 
 $d = $data->get_data();
 
-$routes = array_values( array_map( function( array $data, string $route ) : array {
+$routes = array_map( function( array $data, string $route ) : array {
 	$data['route'] = $route;
 	return $data;
-}, $d['routes'], array_keys( $d['routes'] ) ) );
+}, $d['routes'], array_keys( $d['routes'] ) );
 
 $dir = 'routes';
 $dir = dirname( ABSPATH ) . '/data/rest-api/' . $dir;
@@ -18,7 +18,14 @@ if ( ! file_exists( $dir ) ) {
 	mkdir( $dir, 0777, true );
 }
 
-foreach ( $routes as $i => $item ) {
+foreach ( $routes as $item ) {
+	$i = preg_replace( '#[^a-z0-9]+#', '-', $item['route'] );
+	$i = preg_replace( '#\b[dw]\b#', '', $i );
+	$i = str_replace( 'a-z', '', $i );
+	$i = str_replace( '0-9', '', $i );
+	$i = preg_replace( '#\-+#', '-', $i );
+	$i = trim( $i, '-' );
+
 	$json = json_encode( $item, JSON_PRETTY_PRINT ^ JSON_UNESCAPED_SLASHES );
 
 	file_put_contents( $dir . '/' . $i . '.json', $json );
