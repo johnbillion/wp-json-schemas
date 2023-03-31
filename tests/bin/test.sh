ce6d18faf1036f22130948e860c7f492b0f56b2c
John Blackbourn <john@humanmade.com>
2023-03-31 23:52:52 +0100

    Add tests for REST API responses where the schema is shared between several endpoints.

--- tests/bin/test.sh
+++ tests/bin/test.sh
@@ -10,10 +10,11 @@ function test_missing_properties() {
 }
 
 function validate_schema() {
-	local file="$1"
+	local schema="$1"
+	local file="${2:-$schema}"
 	local base=${file//schemas\//}
 	local base=${base/.json/}
-	./node_modules/.bin/ajv validate -m tests/hyper-schema/hyper-schema.json -r "schemas/**/*.json" -s "$file" -d "tests/data/$base/*.json"
+	./node_modules/.bin/ajv validate -m tests/hyper-schema/hyper-schema.json -r "schemas/**/*.json" -s "$schema" -d "tests/data/$base/*.json"
 }
 
 for file in schemas/*.json
@@ -27,3 +28,6 @@ do
 	test_missing_properties "$file"
 	validate_schema "$file"
 done
+
+# Validate the wp/v2/pages response against the posts schema
+validate_schema "schemas/rest-api/posts.json" "rest-api/pages"
