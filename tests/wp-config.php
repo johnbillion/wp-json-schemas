faccce6b9b8551e747ba04edcc6243d9527882cf
John Blackbourn <john@humanmade.com>
2023-04-01 00:26:56 +0100

    Change this default value.

--- tests/wp-config.php
+++ tests/wp-config.php
@@ -23,7 +23,7 @@ define( 'WP_ENVIRONMENT_TYPE', 'local' );
 // DO NOT use a production database or one that is shared with something else.
 define( 'DB_NAME',     getenv( 'WP_TESTS_DB_NAME' ) ?: 'wordpress_test' );
 define( 'DB_USER',     getenv( 'WP_TESTS_DB_USER' ) ?: 'root' );
-define( 'DB_PASSWORD', getenv( 'WP_TESTS_DB_PASS' ) ?: '' );
+define( 'DB_PASSWORD', getenv( 'WP_TESTS_DB_PASS' ) ?: 'root' );
 define( 'DB_HOST',     getenv( 'WP_TESTS_DB_HOST' ) ?: 'localhost' );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
