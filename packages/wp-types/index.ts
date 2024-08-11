56c97cb70cca92f2e333934377d64791204938d6
John Blackbourn <johnbillion@gmail.com>
2024-08-12 00:07:46 +0100

    Generated TS types.

--- packages/wp-types/index.ts
+++ packages/wp-types/index.ts
@@ -124,6 +124,10 @@ export type WP_REST_API_Search_Results = WP_REST_API_Search_Result[];
  * A collection of user application passwords in a REST API context.
  */
 export type WP_REST_API_Application_Passwords = WP_REST_API_Application_Password[];
+/**
+ * A collection of menu items in a REST API context.
+ */
+export type WP_REST_API_Menu_Items = WP_REST_API_Menu_Item[];
 
 /**
  * WordPress is open source software you can use to create a beautiful website, blog, or app.
@@ -194,6 +198,8 @@ export interface WP {
 		Types: WP_REST_API_Types;
 		Application_Password: WP_REST_API_Application_Password;
 		Application_Passwords: WP_REST_API_Application_Passwords;
+		Menu_Item: WP_REST_API_Menu_Item;
+		Menu_Items: WP_REST_API_Menu_Items;
 		Error: WP_REST_API_Error;
 	};
 }
@@ -3586,6 +3592,111 @@ export interface WP_REST_API_Application_Password {
 	 */
 	last_ip: string | null;
 	_links?: WP_REST_API_Object_Links;
+	[k: string]: unknown;
+}
+/**
+ * A menu item in a REST API context.
+ */
+export interface WP_REST_API_Menu_Item {
+	/**
+	 * The title for the menu item.
+	 */
+	title:
+		| string
+		| {
+				/**
+				 * Title for the menu item, as it exists in the database.
+				 */
+				raw?: string;
+				/**
+				 * HTML title for the menu item, transformed for display.
+				 */
+				rendered?: string;
+				[k: string]: unknown;
+		  };
+	/**
+	 * Unique identifier for the menu item.
+	 */
+	id: number;
+	/**
+	 * The singular label used to describe this type of menu item.
+	 */
+	type_label: string;
+	/**
+	 * The family of objects originally represented.
+	 */
+	type: "taxonomy" | "post_type" | "post_type_archive" | "custom";
+	/**
+	 * A named status for the menu item.
+	 */
+	status: "publish" | "future" | "draft" | "pending" | "private";
+	/**
+	 * The ID for the parent of the menu item.
+	 */
+	parent: number;
+	/**
+	 * Text for the title attribute of the link element for this menu item.
+	 */
+	attr_title: string;
+	/**
+	 * Class names for the link element of this menu item.
+	 */
+	classes: string[];
+	/**
+	 * The description of this menu item.
+	 */
+	description: string;
+	/**
+	 * The DB ID of the nav_menu_item that is this item's menu parent, if any, otherwise 0.
+	 */
+	menu_order: number;
+	/**
+	 * The type of object originally represented, such as "category", "post", or "attachment".
+	 */
+	object: string;
+	/**
+	 * The database ID of the original object this menu item represents, for example the ID for posts or the term_id for categories.
+	 */
+	object_id: number;
+	/**
+	 * The target attribute of the link element for this menu item.
+	 */
+	target: "_blank" | "";
+	/**
+	 * The URL to which this menu item points.
+	 */
+	url: string;
+	/**
+	 * The XFN relationship expressed in the link of this menu item.
+	 */
+	xfn: string[];
+	/**
+	 * Whether the menu item represents an object that no longer exists.
+	 */
+	invalid: boolean;
+	/**
+	 * The terms assigned to the menu item in the nav_menu taxonomy.
+	 */
+	menus: number;
+	/**
+	 * Meta fields.
+	 */
+	meta:
+		| EmptyArray
+		| {
+				[k: string]: unknown;
+		  };
+	_links: WP_REST_API_Object_Links;
+	/**
+	 * The embedded representation of relations. Only present when the '_embed' query parameter is set.
+	 */
+	_embedded?: {
+		/**
+		 * The taxonomy terms for the nav menu item.
+		 */
+		"wp:term": unknown[];
+		[k: string]: unknown;
+	};
 	[k: string]: unknown;
 }
 /**
